9bb5be12cc33a706c5a73b2c0a6bd3b602af3694
steeven-louk <steevenlouk.sl@gmail.com>
2025-03-19 15:35:30 +0100

    ajout des job recent

--- src/app/components/recentJobs.tsx
+++ src/app/components/recentJobs.tsx
@@ -1,10 +1,33 @@
+"use client"
+
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { JobCard } from './jobCard'
+import { getAllJob } from '../services/jobService';
+import { JobCardSkeleton } from './skeletons/job-card-skeleton';
 
 
 
 export const RecentJobs = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [recentJobs, setRecentJobs]=useState([])
+
+    useEffect(() => {
+        const getRecentJob =async()=>{
+             try {
+                    setIsLoading(true);
+                    const data = await getAllJob();
+                    const filteredData = data.slice(0,6)
+                    setRecentJobs(filteredData);
+                  } catch (error) {
+                    // toast.error("Erreur lors de la récupération des jobs");
+                    console.error("❌ Erreur récupération jobs:", error);
+                  } finally {
+                    setTimeout(() => setIsLoading(false), 500);
+                  }
+        }
+        getRecentJob()
+    }, [])
     return (
         <section className='container px-4 md:px-10 mx-auto mt-5 md:my-10'>
             <h1 className='capitalize font-bold text-2xl'>recent jobs available</h1>
@@ -12,12 +35,21 @@ export const RecentJobs = () => {
                 <p className=' text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                 <Link href={"/jobs"} className='underline color-primary font-semibold'>Voir plus</Link>
             </div>
-            <JobCard path={''}/>
-            <JobCard path={''}/>
-            <JobCard path={''} job={undefined}/>
-            <JobCard path={''}/>
-            <JobCard path={''}/>
-            <JobCard path={''}/>
+            {isLoading?(
+            <div className='space-x-4'>
+                {[...Array(4)].map((_,index)=>(
+                    <JobCardSkeleton key={index}/>
+                ))}
+            </div>  )
+            :(
+                recentJobs?.slice(0,6).map((job)=>(
+
+                    <JobCard path={''} key={job?.id} job={job}/>
+                ))
+            )
+
+        }
+
         </section>
     )
 }

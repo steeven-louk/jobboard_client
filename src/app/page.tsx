d9c0ed810a760cc3e01b9f1d4bcef2d536789c3c
steeven-louk <steevenlouk.sl@gmail.com>
2025-03-23 14:41:28 +0100

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ import Heros from "./components/heros"
 import { RecentJobs } from "./components/recentJobs";
 
 export default function Home() {
-
+console.log("next ayth",process.env.NEXTAUTH_URL)
+console.log(process.env.NEXT_PUBLIC_API_URL)
   return (
     <div>
       <Heros/>
